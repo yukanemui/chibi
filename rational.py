5e66773efdb7b9461aff6d15ac9090d4201bb4c3
ICHIKAWA Yuka <21716010@jwu.ac.jp>
2019-10-07 16:24:25 +0900

    added gcd

--- rational.py
+++ rational.py
@@ -3,7 +3,15 @@ class Q(object):
         self.a = a
         self.b = b
     def __repr__(self):
+        if self.b == 1:
+            return str(self.a)
         return f'{self.a}/{self.b}'
+    deft add(self, q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b
+        return Q(...,...)
 
 
 q = Q(1,2)
