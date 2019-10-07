693a09eb3b578fc388ae122d078c88ff600ed164
ICHIKAWA Yuka <21716010@jwu.ac.jp>
2019-10-07 16:49:16 +0900

    integrated int

--- rational.py
+++ rational.py
@@ -1,7 +1,8 @@
 class Q(object):
-    def __init__(self, a,b):
-        self.a = a
-        self.b = b
+    def __init__(self, a,b=1):
+        gcd = math.gcd(a.b)
+        self.a = a//gcd
+        self.b = b//gcd
     def __repr__(self):
         if self.b == 1:
             return str(self.a)
