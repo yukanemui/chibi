73c6f857451d0749d221c57db128a467283b7104
ICHIKAWA Yuka <21716010@jwu.ac.jp>
2019-10-07 16:34:35 +0900

    added +

--- rational.py
+++ rational.py
@@ -6,14 +6,17 @@ class Q(object):
         if self.b == 1:
             return str(self.a)
         return f'{self.a}/{self.b}'
-    deft add(self, q):
+    deft __add__(self, q):
         a = self.a
         b = self.b
         c = q.a
         d = q.b
-        return Q(...,...)
+        return Q(a*d+b*c,b*d)
 
 
 q = Q(1,2)
 q = Q(1,3)
-print(q1.add(q2))
+print(q1 + q2)
+print(q1 - q2)
+print(q1 * q2)
+print(q1 / q2)
