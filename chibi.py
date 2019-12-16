d93a2239c67e0e2222285ccaf0a4ae60fc598ce8
ICHIKAWA Yuka <21716010@jwu.ac.jp>
2019-12-16 16:28:19 +0900

    added_reper_

--- chibi.py
+++ chibi.py
@@ -143,6 +143,37 @@ assert e.eval({}) == 0class Binary(Expr):
         e = conv(tree)
         #print('env', env)
         print(e.eval(env))def main():
+        class Lambda(Expr):
+            __slots__ = ['name', 'body']
+            def __init__(self, name, body):
+                self.name = name
+                self.body = body
+            def __repr__(self):
+                return f'λ{self.name} . {str(self.body)}'
+            def eval(self, env):
+                return selfdef copy(env): #環境をコピーすることでローカルスコープを作る
+            newenv = {}
+            for x in env.keys():
+                newenv[x] = env[x]
+            return envclass FuncApp(Expr):
+            __slots__ = ['func', 'param']
+            def __init__(self, func: Lambda, param):
+                self.func = func
+                self.param = Expr.new(param)
+            def __repr__(self):
+                return f'({repr(self.func)}) ({repr(self.param)})'    def eval(self, env):
+                f = self.func.eval(env)
+                v = self.param.eval(env)  # パラメータを先に評価する
+                name = f.name # Lambda の変数名をとる
+                env = copy(env)  # 環境をコピーすることでローカルスコープを作る
+                env[name] = v   # 環境から引数を渡す
+                return f.body.eval(env)def conv(tree):
+            if tree == 'Block':
+                return conv(tree[0])
+            if tree == 'FuncDecl':   # この２行を追加します
+                return Assign(str(tree[0]), Lambda(str(tree[1]), conv(tree[2])))
+            if tree == 'FuncApp':   # この２行を追加します
+                return FuncApp(conv(tree[0]), conv(tree[1]))        
     try:
         env = {}
         while True:
