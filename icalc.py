c4db6b02e5aafada4b1b946ea7a739085eff57b7
ICHIKAWA Yuka <21716010@jwu.ac.jp>
2020-01-20 16:34:59 +0900

    added_reper_

--- icalc.py
+++ icalc.py
@@ -121,3 +121,18 @@ class Mul(Binary):
                                         return self.then.eval(env)
                                     else:
                                         return self.else_.eval(env)
+                                    class Lambda(Expr):
+                                        __slots__ = ['name', 'body']
+                                        def __init__(self, name, body):
+                                            self.name = name
+                                            self.body = body
+                                        def __repr__(self):
+                                            return f'λ{self.name} . {str(self.body)}'
+                                        def eval(self, env):
+                                            return self
+                                    def copy(env): #環境をコピーすることでローカルスコープを作る
+                                        newenv = {}
+                                        for x in env.keys():
+                                            newenv[x] = env[x]
+                                        return newenv
+                                    
