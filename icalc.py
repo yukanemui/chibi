abe4816f9ce043889e73718cb86dd11479009f2e
ICHIKAWA Yuka <21716010@jwu.ac.jp>
2020-01-20 16:31:06 +0900

    added_reper_

--- icalc.py
+++ icalc.py
@@ -1,11 +1,46 @@
 import pegpy
-peg = pegpy.grammar('''
-Expression = Product (^{ '+' Product #Add })*
-Product = Value (^{ '*' Value #Mul })*
-Value = { [0-9]+ #Int }
-''')
+#from pegpy.tpeg import ParseTree
+peg = pegpy.grammar('chibi.tpeg')
 parser = pegpy.generate(peg)
-t = parser('1+2*3')
-print(repr(t))
-t = parser('@2')
-print(repr(t))
+'''
+tree = parser('1+2*3')
+print(repr(tree))
+tree = parser('1@2*3')
+print(repr(tree))
+'''
+class Expr(object):
+    @classmethod
+    def new(cls, v):
+        if isinstance(v, Expr):
+            return v
+        return Val(v)
+class Val(Expr):
+    __slots__ = ['value']
+    def __init__(self, value):
+        self.value = value
+    def __repr__(self):
+        return f'Val({self.value})'
+    def eval(self, env: dict):
+        return self.value
+e = Val(0)
+assert e.eval({}) == 0
+class Binary(Expr):
+    __slots__ = ['left', 'right']
+    def __init__(self, left, right):
+        self.left = Expr.new(left)
+        self.right = Expr.new(right)
+    def __repr__(self):
+        classname = self.__class__.__name__
+        return f'{classname}({self.left},{self.right})'
+class Add(Binary):
+    __slots__ = ['left', 'right']
+    def eval(self, env: dict):
+        return self.left.eval(env) + self.right.eval(env)
+class Sub(Binary):
+    __slots__ = ['left', 'right']
+    def eval(self, env: dict):
+        return self.left.eval(env) - self.right.eval(env)
+class Mul(Binary):
+    __slots__ = ['left', 'right']
+    def eval(self, env: dict):
+        return self.left.eval(env) * self.right.eval(env)
